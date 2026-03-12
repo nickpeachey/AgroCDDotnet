b4d4db45722508e1d2e95f3e5420e0b4a2e84bd0
Nick Peachey <nick.peachey@gmail.com>
2026-03-12 16:56:56 +0000

    added change

--- AgroCDDotnet.Api/Todos/TodoContracts.cs
+++ AgroCDDotnet.Api/Todos/TodoContracts.cs
@@ -3,3 +3,5 @@ namespace AgroCDDotnet.Api.Todos;
 public sealed record CreateTodoRequest(string Title, bool IsCompleted);
 
 public sealed record UpdateTodoRequest(string Title, bool IsCompleted);
+
+public sealed record DeleteTodoRequest(Guid Id);
